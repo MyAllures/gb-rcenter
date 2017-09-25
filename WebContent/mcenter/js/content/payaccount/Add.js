77b4810ea6440803f8f462ba516a85ca9b90816a
leo <leo@dawoo.com>
2017-09-25 15:12:49 +0800

    公司入款账号--添加gif修复

--- WebContent/mcenter/js/content/payaccount/Add.js
+++ WebContent/mcenter/js/content/payaccount/Add.js
@@ -181,7 +181,7 @@ define(['common/BaseEditPage', 'jqFileInput', 'css!themesCss/fileinput/fileinput
                 mainClass: "input-group",
                 removeLabel: window.top.message.content['floatPic.file.upload.remove'],
                 browseLabel: window.top.message.content['floatPic.file.upload.browse'] + '&hellip;',
-                allowedFileExtensions: ['jpg', 'jpeg', 'png'],
+                allowedFileExtensions: ['jpg', 'jpeg', 'png','gif'],
                 msgInvalidFileExtension: window.top.message.content['floatPic.file.upload.msgInvalidFileExtension'],
                 msgValidationError: window.top.message.content['floatPic.file.upload.msgValidationError'],
                 msgSizeTooLarge: window.top.message.content['floatPic.file.upload.msgSizeTooLarge'],
