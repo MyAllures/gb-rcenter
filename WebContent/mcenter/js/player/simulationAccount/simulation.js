e75b899bcef8c0c79f8637e8c0745a46b5fa24ee
leo <leo@dawoo.com>
2017-10-04 09:26:16 +0800

    添加冻结账号与模拟账号

---
+++ WebContent/mcenter/js/player/simulationAccount/simulation.js
@@ -0,0 +1,50 @@
+define(['common/BaseListPage','common/BaseEditPage'], function(BaseListPage,BaseEditPage) {
+
+    return BaseListPage.extend({
+        /**
+         * 初始化及构造函数，在子类中采用
+         * this._super();
+         * 调用
+         */
+        selectedIds:null,
+        init : function() {
+            this._super();
+        },
+        bindEvent:function()
+        {
+            this._super();
+            // 列表详情事件绑定
+            this.initShowDetail();
+
+        },
+        onPageLoad: function () {
+            this._super();
+        },
+        unableAccount:function () {
+            var _msg;
+            _msg="<div style='text-align: center'><div style='color: red'><i class='fa fa-exclamation-circle'></i>停用后不可重新启用！</div>确认停用该虚拟账号吗？</div>"
+            window.top.topPage.showConfirmMessage(_msg, function (confirm) {
+                if (confirm){
+                    var id=$("input[name='id']").val();
+                    window.top.topPage.ajax({
+                        url: root + '/simulationAccount/disablePlayer.html',
+                        data: {"search.id":id},
+                        dataType:'json',
+                        type: "POST",
+                        success: function (data) {
+
+                        }
+                    });
+                }
+            })
+        },
+        getIds:function (e,opt) {
+            this.selectedIds = this.getSelectIdsArray(e,opt).join(",");
+            return true
+        },
+
+        getSelectedIds:function () {
+            return this.selectedIds;
+        }
+    });
+});
