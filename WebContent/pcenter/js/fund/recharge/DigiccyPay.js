c510dd5ae4694b2b9b78d6ed6b0c4516a48205f3
cherry <cherry1012@mail.com>
2017-10-05 09:16:53 +0800

    ==修改转账上限

---
+++ WebContent/pcenter/js/fund/recharge/DigiccyPay.js
@@ -0,0 +1,36 @@
+/**
+ * 线上支付
+ */
+define(['common/BaseEditPage'], function (BaseEditPage) {
+    return BaseEditPage.extend({
+        init: function () {
+            this.formSelector = "form[name=digiccyPayForm]";
+            this.super(this.formSelector);
+        },
+        /**
+         * 页面加载后加载
+         */
+        onPageLoad: function () {
+            this._super();
+            var clip = new ZeroClipboard($('button[name="copy"]'));
+            clip.on('aftercopy', function (e) {
+                e.currentTarget = e.target;
+                page.showPopover(e, {}, 'success', window.top.message.fund_auto['复制成功'], true);
+            });
+        },
+        /**
+         * 生成地址
+         */
+        newAddress: function (e, option) {
+
+        },
+        /**
+         * 兑换
+         * @param e
+         * @param option
+         */
+        exchange: function (e, option) {
+
+        }
+    })
+});
