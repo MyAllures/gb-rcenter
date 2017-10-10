965f1a7c23c9d05ab5bd20c3461a85e32397bf98
cherry <cherry1012@mail.com>
2017-10-10 11:33:41 +0800

    ==数字货币

--- WebContent/pcenter/js/fund/recharge/DigiccyPay.js
+++ WebContent/pcenter/js/fund/recharge/DigiccyPay.js
@@ -24,7 +24,25 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
         newAddress: function (e, option) {
             var currency = option.currency;
             window.top.topPage.ajax({
-
+                url: root + "/fund/recharge/digiccy/newAddress.html",
+                data: {'currency': currency},
+                dataType: 'json',
+                success: function (data) {
+                    var address = data.address;
+                    var addressQrcodeUrl = data.addressQrcodeUrl;
+                    if (address) {
+                        e.page.showPopover(e, option, 'success', '生成地址成功', true);
+                        window.setTimeout(function () {
+                            var html = $("#addressRender").render({
+                                'address': address,
+                                'addressQrcodeUrl': addressQrcodeUrl
+                            });
+                            $("#" + currency).html(html);
+                        }, 1000);
+                    } else {
+                        e.page.showPopover(e, option, 'warning', '生成地址失败请稍后再试！', true);
+                    }
+                }
             })
         },
         /**
@@ -34,6 +52,30 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
          */
         exchange: function (e, option) {
             var currency = option.currency;
+            window.top.topPage.ajax({
+                url: root + "/fund/recharge/digiccy/exchange.html",
+                data: {'currency': currency},
+                dataType: 'json',
+                success: function (data) {
+
+                }
+            })
+        },
+        /**
+         * 刷新金额
+         * @param e
+         * @param option
+         */
+        refresh: function (e, option) {
+            var currency = option.currency;
+            window.top.topPage.ajax({
+                url: root + "/fund/recharge/digiccy/refresh.html",
+                data: {'currency': currency},
+                dataType: 'json',
+                success: function (data) {
+                    $(e.currentTarget).prev(".orange").text()
+                }
+            })
         }
     })
 });
