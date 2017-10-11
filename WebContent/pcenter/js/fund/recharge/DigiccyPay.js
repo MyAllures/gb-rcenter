39e6c381bb093c99dd5e8585e1fc8cb3474c3a2b
cherry <cherry1012@mail.com>
2017-10-11 15:04:47 +0800

    ==数字货币

--- WebContent/pcenter/js/fund/recharge/DigiccyPay.js
+++ WebContent/pcenter/js/fund/recharge/DigiccyPay.js
@@ -1,11 +1,11 @@
 /**
- * 线上支付
+ * 数字货币支付
  */
 define(['common/BaseEditPage'], function (BaseEditPage) {
     return BaseEditPage.extend({
         init: function () {
             this.formSelector = "form[name=digiccyPayForm]";
-            this.super(this.formSelector);
+            this._super(this.formSelector);
         },
         /**
          * 页面加载后加载
@@ -29,19 +29,21 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
                 dataType: 'json',
                 success: function (data) {
                     var address = data.address;
-                    var addressQrcodeUrl = data.addressQrcodeUrl;
                     if (address) {
                         e.page.showPopover(e, option, 'success', '生成地址成功', true);
                         window.setTimeout(function () {
-                            var html = $("#addressRender").render({
-                                'address': address,
-                                'addressQrcodeUrl': addressQrcodeUrl
-                            });
+                            var html = $("#addressRender").render({data: data});
                             $("#" + currency).html(html);
+                            var clip = new ZeroClipboard($('button[name="copy"]'));
+                            clip.on('aftercopy', function (e) {
+                                e.currentTarget = e.target;
+                                page.showPopover(e, {}, 'success', window.top.message.fund_auto['复制成功'], true);
+                            });
                         }, 1000);
                     } else {
                         e.page.showPopover(e, option, 'warning', '生成地址失败请稍后再试！', true);
                     }
+                    $(e.currentTarget).unlock();
                 }
             })
         },
@@ -58,6 +60,7 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
                 dataType: 'json',
                 success: function (data) {
 
+                    $(e.currentTarget).unlock();
                 }
             })
         },
@@ -74,6 +77,7 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
                 dataType: 'json',
                 success: function (data) {
                     $(e.currentTarget).prev(".orange").text(data.amount);
+                    $(e.currentTarget).unlock();
                 }
             })
         }
