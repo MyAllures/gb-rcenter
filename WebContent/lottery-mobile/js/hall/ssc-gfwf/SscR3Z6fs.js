8016259d7ccf9dabcfdde81408cabb3bd13a9206
cena <cena@dawoo.com>
2017-10-13 21:05:07 +0800

    任选三

--- WebContent/lottery-mobile/js/hall/ssc-gfwf/SscR3Z6fs.js
+++ WebContent/lottery-mobile/js/hall/ssc-gfwf/SscR3Z6fs.js
@@ -20,9 +20,9 @@ define(['site/hall/ssc-gfwf/AllSsc', 'site/plugin/template','RangeSlider'], func
             $("#R3").show();
             $("span.x_1.gfwf-tit").text("任选三");
             $(".s-title.title1 span").text("任选三");
-            $(".s-title.title2 span").text("组三复式");
-            $(".x_3.gfwf-playName").text("组三复式");
-            $("a[data-code='ssc_renxuan3_z3fs']").addClass("mui-active");
+            $(".s-title.title2 span").text("组六复式");
+            $(".x_3.gfwf-playName").text("组六复式");
+            $("a[data-code='ssc_renxuan3_z6fs']").addClass("mui-active");
 
         },
 
@@ -30,32 +30,29 @@ define(['site/hall/ssc-gfwf/AllSsc', 'site/plugin/template','RangeSlider'], func
         /**
          * 注数-任选二 / 时时彩与11选5共用注数方法
          */
-        zhushu_ssc_renxuan3_z3fs :function(){
+        zhushu_ssc_renxuan3_z6fs :function(){
 
-            var zuArr = [];
+            var fuShiArr = [], newArr = [];
             $.each($("a.n-btn.mui-active"), function (index, value) {
-                zuArr.push($.trim($(this).html()));
+                fuShiArr.push($.trim($(this).html()));
             });
-            var tempArr = [];
-            for (var i = 0; i < zuArr.length; i++) {
-                for (var i1 = 0; i1 < zuArr.length; i1++) {
-                    if (zuArr[i] != zuArr[i1]) {
-                        tempArr.push(zuArr[i] + "" + zuArr[i1]);
-                    }
-                }
-
+            var zlLength = fuShiArr.length;
+            if (zlLength < 3) {
+                return 0;
             }
 
+            newArr = this.getZuLiuNewArrs(fuShiArr);
+            var zhushu = newArr.length;
             // 选取选中checkbox
             var checkArr = this.getCheckboxValue();
             var shu = this.getFlagArrs(checkArr, 3).length;
-            return tempArr.length * shu;
+            return zhushu * shu;
         },
 
         /**
          * 直选复式
          */
-        content_ssc_renxuan3_z3fs : function(){
+        content_ssc_renxuan3_z6fs : function(){
 
             var zuArr = [];
             var checkStrArr = [];
@@ -77,21 +74,24 @@ define(['site/hall/ssc-gfwf/AllSsc', 'site/plugin/template','RangeSlider'], func
         /**
          * 随机算法-任二直选复式
          */
-        random_ssc_renxuan3_z3fs : function() {
+        random_ssc_renxuan3_z6fs : function() {
 
             var arrTemp = [];
-            while(arrTemp.length < 2){
+            while(arrTemp.length < 3){
                 var random_1 = parseInt(Math.random() * 10);
                 var random_2 = parseInt(Math.random() * 10);
-                if(random_1 != random_2){
+                var random_3 = parseInt(Math.random() * 10);
+                if(random_1 != random_2 && random_2 != random_3 && random_1 != random_3){
                     arrTemp.push(random_1);
                     arrTemp.push(random_2);
+                    arrTemp.push(random_3);
                 }
             }
 
             $("a.n-btn.mui-active").removeClass("mui-active");
             $("a.n-btn").eq(random_1).addClass("mui-active");
             $("a.n-btn").eq(random_2).addClass("mui-active");
+            $("a.n-btn").eq(random_3).addClass("mui-active");
         },
 
         /**
@@ -183,7 +183,42 @@ define(['site/hall/ssc-gfwf/AllSsc', 'site/plugin/template','RangeSlider'], func
                 }
             });
             return result;
+        },
+
+        //组六复式
+         getZuLiuNewArrs: function(zuXuanArr) {
+            var tempArr = [], zxArr = [];
+            zxArr = zuXuanArr;
+            for (var i = 0; i < zxArr.length; i++) {
+                for (var i1 = 0; i1 < zxArr.length; i1++) {
+                    for (var i2 = 0; i2 < zxArr.length; i2++) {
+                        if (zxArr[i] != zxArr[i1] && zxArr[i1] != zxArr[i2] && zxArr[i] != zxArr[i2]) {
+                            var sortArr = [];
+                            sortArr.push(zxArr[i]);
+                            sortArr.push(zxArr[i1]);
+                            sortArr.push(zxArr[i2]);
+                            sortArr.sort();
+                            tempArr.push(sortArr.join(""));
+                        }
+                    }
+                }
+            }
+            tempArr = tempArr.uniqueArr();
+            return tempArr;
         }
 
     });
 });
+
+//去掉数组重复
+Array.prototype.uniqueArr = function () {
+    var temp = new Array();
+    this.sort();
+    for(i = 0; i < this.length; i++) {
+        if( this[i] == this[i+1]) {
+            continue;
+        }
+        temp[temp.length]=this[i];
+    }
+    return temp;
+}
