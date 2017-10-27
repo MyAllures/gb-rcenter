3ded214105329d919c6fda45da76175dbe87d001
diego <[diego@dawoo.com]>
2017-10-27 17:58:21 +0800

    添加  全  清 按钮

--- WebContent/lottery/js/hall/ssc/SscGfwf.js
+++ WebContent/lottery/js/hall/ssc/SscGfwf.js
@@ -911,7 +911,7 @@ define(['site/hall/ssc/PlayWay','site/plugin/template','range','css!themesCss/jq
             objArr.each(function () {
                 $(this).removeClass("acti");
                 var num = parseInt($(this).find("i").html());
-                if ($.inArray(num, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]) >= 0) {
+                if ($.inArray(num, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]) >= 0) {
                     $(this).addClass("acti");
                 }
             });
