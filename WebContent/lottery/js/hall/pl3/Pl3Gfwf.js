2fce053949fbdf9b97444a4266260d4517a494c3
diego <[diego@dawoo.com]>
2017-10-27 19:39:45 +0800

    添加  全  清 按钮

--- WebContent/lottery/js/hall/pl3/Pl3Gfwf.js
+++ WebContent/lottery/js/hall/pl3/Pl3Gfwf.js
@@ -913,7 +913,7 @@ define(['site/hall/pl3/Pl3PlayWay','site/plugin/template','range','css!themesCss
             objArr.each(function () {
                 $(this).removeClass("acti");
                 var num = parseInt($(this).find("i").html());
-                if ($.inArray(num, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]) >= 0) {
+                if ($.inArray(num, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]) >= 0) {
                     $(this).addClass("acti");
                 }
             });
