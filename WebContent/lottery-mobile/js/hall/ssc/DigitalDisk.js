c23752a10b2e4b1b934e8653f3eb91213d512351
Fei <fei@dawoo.com>
2017-11-24 15:03:02 +0800

    ==纯彩票站-Android投注切换

--- WebContent/lottery-mobile/js/hall/ssc/DigitalDisk.js
+++ WebContent/lottery-mobile/js/hall/ssc/DigitalDisk.js
@@ -37,6 +37,9 @@ define(['site/hall/ssc/PlayWay-xywf', 'site/plugin/template'], function (PlayWay
             $(".s-title.title1 span").text("数字盘");
             $(".s-title.title2 span").text("数字盘");
             $("#toobarTitle").text("传统玩法-数字盘");
+            if (this.os == 'app_android' && isLotterySite == 'true') {
+                window.gamebox.setTitle('传统玩法-数字盘');
+            }
             $("a[data-code='szp'] span").text("数字盘");
         }
 
--- WebContent/lottery-mobile/js/hall/ssc/Span.js
+++ WebContent/lottery-mobile/js/hall/ssc/Span.js
@@ -16,6 +16,11 @@ define(['site/hall/ssc/PlayWay-xywf','site/plugin/template'], function (PlayWay,
             $(".s-title.title1 span").text("跨度");
             $(".s-title.title2 span").text(betCode);
             $("#toobarTitle").text("传统玩法-跨度");
+
+            if (this.os == 'app_android' && isLotterySite == 'true') {
+                window.gamebox.setTitle('传统玩法-跨度');
+            }
+
             if(betCode =="ssc_kaudu"){
                 $("a[data-code='前三组选六']").addClass("mui-active");
             }else{
