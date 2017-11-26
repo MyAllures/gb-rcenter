7ce0133c8555e857b5f0fe6c3842b42c68dae8a1
ed <edon@dawoo.com>
2017-11-26 17:53:07 +0800

    新增mobile-v3首页进入游戏

--- WebContent/mobile-v3/js/game/GoGame.js
+++ WebContent/mobile-v3/js/game/GoGame.js
@@ -191,15 +191,6 @@ function showGameLoading () {
     $('div.game-mask').show();
 }
 
-// 获取cookie
-function getCookie(name) {
-    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-    if(arr=document.cookie.match(reg))
-        return unescape(arr[2]);
-    else
-        return null;
-}
-
 /**
  * 免转登录
  **/
