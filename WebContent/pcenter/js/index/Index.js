7b8b06b8ea1d46b0bb7f258693ea6a0beda85435
younger <younger.h@mail.com>
2017-12-10 17:14:59 +0800

    还原代码

--- WebContent/pcenter/js/index/Index.js
+++ WebContent/pcenter/js/index/Index.js
@@ -45,34 +45,6 @@ define([], function () {
             this.bindEvent();
             this.checkBrowser();
             this.onPageLoad();
-            if(isLotterySite){
-                this.initBackgroundBlur();
-            }
-        },
-
-        initBackgroundBlur:function () {
-            var $avatarEl = $('.banner');
-            var $avatarHolderEl = $('.banner-bg');
-            var images = [];
-            var avatars = [];
-
-            // Avatar - based backgrounds
-            $avatarEl.each(function(){
-                var $avatar = $(this);
-                var avatarImage = $avatar.find('img').first().attr('src');
-                avatars.push(avatarImage);
-                $avatar.click(function(e){
-                    e.preventDefault();
-
-                    $avatarHolderEl.backgroundBlur(avatarImage);
-                });
-            });
-
-            $avatarHolderEl.backgroundBlur({
-                imageURL : avatars[0], // URL to the image that will be used for blurring
-                blurAmount : 10, // Amount of blur (higher amount degrades browser performance)
-                imageClass : 'avatar-blur' // CSS class that will be applied to the image and to the SVG element,
-            });
         },
         bindEvent: function () {
             var _this = this;
