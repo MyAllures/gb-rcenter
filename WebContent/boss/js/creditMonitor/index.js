d1d0e06977c4f6e9774cc13b5f143719ab91191a
back <back@dawoo.com>
2017-12-12 20:57:29 +0800

    买分：修改大额监控按站点ＩＤ查询报错

---
+++ WebContent/boss/js/creditMonitor/index.js
@@ -0,0 +1,32 @@
+/**
+ * 买分——大额监控
+ */
+define(['common/BaseListPage','bootstrapswitch'], function (BaseListPage,bootstrapswitch) {
+
+    return BaseListPage.extend({
+        /**
+         * 初始化及构造函数，在子类中采用
+         * this._super();
+         * 调用
+         */
+        init: function () {
+            this._super();
+        },
+
+        /**
+         * 当前对象事件初始化函数
+         */
+        bindEvent : function() {
+            this._super();
+        },
+        /**
+         * 保存或更新前验证
+         * @param e   事件对象
+         * @return 验证是否通过
+         */
+        validateForm: function (e) {
+            var $form = $(window.top.topPage.getCurrentForm(e));
+            return !$form.valid || $form.valid();
+        }
+    });
+});
