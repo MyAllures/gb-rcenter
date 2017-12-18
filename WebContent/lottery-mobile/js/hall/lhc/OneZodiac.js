72f701b33ae503e393c409f86875af6476d22310
diego <[diego@dawoo.com]>
2017-12-18 19:56:17 +0800

    一肖/尾 回退 一肖

--- WebContent/lottery-mobile/js/hall/lhc/OneZodiac.js
+++ WebContent/lottery-mobile/js/hall/lhc/OneZodiac.js
@@ -1,36 +1,8 @@
 define(['site/hall/lhc/PlayWay-xywf'], function (PlayWay) {
     return PlayWay.extend({
         init: function () {
-            this.showTable("一肖/尾","传统玩法-一肖/尾","tema",$("#tema"),"");
+            this.showTable("一肖","传统玩法-一肖","tema",$("#tema"),"");
             this._super();
-        },
-        /**
-         * 获取赔率
-         */
-        getOdds: function () {
-            var dataCode=$("a.selected-btn.main.mui-active").attr("data-code");
-            var url = root + '/' + this.type + '/' + this.code + '/' + dataCode + 'Odd.html';
-            var _this = this;
-            mui.ajax(url, {
-                dataType: 'json',
-                type: 'POST',
-                success: function (data) {
-                    $("#bet-table-list td[data-bet-num]").each(function () {
-                        var betNum = $(this).attr('data-bet-num');
-                        var bet = data[0][betNum];
-                        $(this).attr("data-odds", bet.odd);
-                        $(this).attr("data-bet-code", bet.betCode);
-                        $(this).children("span[name=odd]").text(bet.odd);
-                    }),
-                    $("#bet-table-list-mantissa td[data-bet-num]").each(function () {
-                        var betNum = $(this).attr('data-bet-num');
-                        var bet = data[1][betNum];
-                        $(this).attr("data-odds", bet.odd);
-                        $(this).attr("data-bet-code", bet.betCode);
-                        $(this).children("span[name=odd]").text(bet.odd);
-                    })
-                }
-            })
         }
     });
 });
