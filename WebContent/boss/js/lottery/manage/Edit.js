e17a487144fdf9e03e4d7e7f4e1dc16fb67339e3
rambo <rambo@dawoo.com>
2017-12-18 10:53:18 +0800

    彩种同步添加全选修改

--- WebContent/boss/js/lottery/manage/Edit.js
+++ WebContent/boss/js/lottery/manage/Edit.js
@@ -12,6 +12,18 @@ define(['common/BaseEditPage', 'UE.I18N.'+window.top.language], function (BaseEd
         },
         onPageLoad:function(){
             this._super();
+        },
+        selectAll:function (e) {
+            $("input[name=siteLottery]").each(function(){
+                $(this).prop("checked",true);
+            })
+            $(e.currentTarget).unlock();
+        },
+        selectNoAll:function (e) {
+            $("input[name=siteLottery]").each(function(){
+                $(this).prop("checked",false);
+            })
+            $(e.currentTarget).unlock();
         }
     })
 });
