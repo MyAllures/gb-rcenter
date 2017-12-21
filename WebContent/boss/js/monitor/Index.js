51db59682cd644503ef97e911b4b3eec3088bd17
tony <tony@wwb.so>
2017-12-21 16:23:19 +0800

    监控数据可排序

--- WebContent/boss/js/monitor/Index.js
+++ WebContent/boss/js/monitor/Index.js
@@ -16,6 +16,49 @@ define(['common/BaseListPage'], function (BaseListPage) {
          */
         bindEvent: function () {
             this._super();
+
+        },
+        /**
+         * 页面加载事件函数
+         */
+        onPageLoad: function () {
+            this._super();
+            //自定义下拉选择事件
+            this.sorTable();
+        },
+        /**
+         * 排序
+         */
+        sorTable: function () {
+            var _this=this;
+            $(_this.formSelector + ' th').click(function(){debugger;
+                var table = $(this).parents('table').eq(0)
+                var rows = table.find('tr:gt(0)').toArray().sort(_this.comparer($(this).index()))
+                this.asc = !this.asc
+                if (!this.asc){rows = rows.reverse()}
+                for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+            })
+        },
+        /**
+         * 比较
+         * @param index
+         * @returns {Function}
+         */
+        comparer : function (index) {
+            var _this=this;
+            return function(a, b) {
+                var valA = _this.getCellValue(a, index), valB = _this.getCellValue(b, index)
+                return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+            }
+        },
+        /**
+         * 取值
+         * @param row
+         * @param index
+         * @returns {*|jQuery}
+         */
+        getCellValue: function (row, index) {
+            return $(row).children('td').eq(index).text()
         },
         /**
          * 刷新监控数据
