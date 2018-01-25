ab7a35cb87ab03ce67d606eebd3e8ea4ce6336aa
pack <pack@dawoo.com>
2018-01-25 11:27:00 +0800

    api/游戏数据同步进度　功能

---
+++ WebContent/boss/js/apiGameLog/Edit.js
@@ -0,0 +1,21 @@
+/**
+ * Created by e
+ */
+define(['common/BaseEditPage'], function (BaseEditPage) {
+    return BaseEditPage.extend({
+
+        init: function () {
+            this._super();
+        },
+
+        bindEvent: function () {
+            this._super();
+        },
+
+        onPageLoad: function () {
+            this._super();
+            var _this = this;
+        }
+
+});
+});
