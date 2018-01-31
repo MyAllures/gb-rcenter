3b8c7d1df3e2e44649f52e629ff8081454d5bad8
mical <mical@dawoo.com>
2018-01-31 10:09:05 +0800

    站长中心玩家取款审核异常脚本处理

--- WebContent/mcenter/js/fund/deposit/company/Index.js
+++ WebContent/mcenter/js/fund/deposit/company/Index.js
@@ -91,10 +91,14 @@ define(['common/BaseListPage', 'gb/share/ListFiltersPage','jsrender'], function 
         renderData:function (data) {
             var _this=this;
             var $result = $("#editable tbody", _this.formSelector);
-            var json = JSON.parse(data);
-            if(json.result) {
-                var html = $("#VPlayerDepositListVo",_this.formSelector).render({data:json.result});
-                $result.html(html);
+            try {
+                var json = JSON.parse(data);
+                if(json.result) {
+                    var html = $("#VPlayerDepositListVo",_this.formSelector).render({data:json.result});
+                    $result.html(html);
+                }
+            }catch(err) {
+                console.info(err);
             }
         },
 
