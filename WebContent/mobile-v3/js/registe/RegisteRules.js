16b82ed225b26b849803bd39433352c6816efebd
cherry <cherry1012@mail.com>
2018-02-13 17:39:54 +0800

    ==存款公共js引入带版本号

--- WebContent/mobile-v3/js/registe/RegisteRules.js
+++ WebContent/mobile-v3/js/registe/RegisteRules.js
@@ -5,8 +5,6 @@ $(function(){
     var options = {
         /*主页面滚动指定容器，可自行指定范围*/
         containerScroll: '.mui-content.mui-scroll-wrapper',
-        /*右侧菜单上下滚动，可自行指定范围*/
-        rightMenuScroll: '.mui-scroll-wrapper.mui-assets',
         /*禁用侧滑手势指定样式*/
         disabledHandSlip: ['mui-off-canvas-left']
     };
