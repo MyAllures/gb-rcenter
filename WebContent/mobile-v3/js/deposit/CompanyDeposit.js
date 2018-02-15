c219777fbc895e78e098075c6e61af8e68fc5b87
cherry <cherry1012@mail.com>
2018-02-15 20:30:40 +0800

    ==修改手机端键盘在输入框上面问题

--- WebContent/mobile-v3/js/deposit/CompanyDeposit.js
+++ WebContent/mobile-v3/js/deposit/CompanyDeposit.js
@@ -1,4 +1,11 @@
 $(function(){
+    window.addEventListener("resize", function() {
+        if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+            window.setTimeout(function() {
+                document.activeElement.scrollIntoViewIfNeeded();
+            },0);
+        }
+    })
 });
 /**选择日期组件*/
 function selectionDate() {
--- WebContent/mobile-v3/js/deposit/ThirdPartyPay.js
+++ WebContent/mobile-v3/js/deposit/ThirdPartyPay.js
@@ -1,4 +1,11 @@
 $(function(){
+    window.addEventListener("resize", function() {
+        if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+            window.setTimeout(function() {
+                document.activeElement.scrollIntoViewIfNeeded();
+            },0);
+        }
+    })
 });
 
 /**数字货币支付 切换币种*/
