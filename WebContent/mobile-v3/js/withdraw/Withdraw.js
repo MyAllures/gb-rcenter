f6924b9a848e78d1e743a48fc4b553ef1b19b239
legend <legend@wwb.so>
2018-02-23 11:37:00 +0800

    mobile-v3 取款稽核页面的js修改横纵向滚动，和v3资金记录页面重做

--- WebContent/mobile-v3/js/withdraw/Withdraw.js
+++ WebContent/mobile-v3/js/withdraw/Withdraw.js
@@ -2,9 +2,7 @@
  * Created by legend on 18-2-22.
  */
 $(function(){
-    var options={horizontalVerticalScroll:'.mui-scroll-wrapper'}
+    var options={horizontalVerticalScroll:['.mui-scroll-wrapper']}
     muiInit(options);
-    muiScrollXY()
-
 });
 
