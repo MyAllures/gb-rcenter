ef4cc9967ff313c39c0176d2bb939a880bbc7b48
cherry <cherry1012@mail.com>
2018-02-25 11:51:04 +0800

    修改验证码提示信息地方

--- WebContent/pcenter/js/fund/recharge/AtmCounterFirst.js
+++ WebContent/pcenter/js/fund/recharge/AtmCounterFirst.js
@@ -43,18 +43,6 @@ define(['site/fund/recharge/CommonRecharge', 'site/fund/recharge/RealName'], fun
                 $(_this.formSelector + " span.fee").hide();
                 _this.changeAmountMsg();
             });
-
-            //修改验证码提示信息的地方
-            $(this.formSelector).on("validate", "[name='code']", function (e, message) {
-                if (message) {
-                    $(_this.formSelector + " span[name=codeTitle]").html("<span class=\"tips orange\"><i class=\"mark plaintsmall\"></i>" + message + "</span>");
-                    e.result = true;
-                } else {
-                    $(_this.formSelector + " span[name=codeTitle]").html("<i class='mark successsmall'></i>");
-                    $(_this.formSelector + " [name='code']").removeClass("error");
-                    e.result = false;
-                }
-            });
         },
         /**
          * 立即存款
