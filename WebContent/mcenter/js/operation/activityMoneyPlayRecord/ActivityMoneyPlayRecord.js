7b6362a9bf1bd1d0cf6ce1cb381dac1ae98ee4c5
steffan <steffan@dawoo.com>
2018-03-08 10:37:35 +0800

    红包记录

---
+++ WebContent/mcenter/js/operation/activityMoneyPlayRecord/ActivityMoneyPlayRecord.js
@@ -0,0 +1,37 @@
+define(['common/BaseListPage'], function(BaseListPage) {
+
+    return BaseListPage.extend({
+        /**
+         * 初始化及构造函数，在子类中采用
+         * this._super();
+         * 调用
+         */
+        init : function() {
+            this._super();
+            this.formSelector = "form[name='playerOnlineForm']";
+            //两个from，两个分页，所以要分两次查询，第一次请求查询出一个form的数据，页面加载完成初始化时，触发另一个form的查询
+            $("button:eq(2)").click();
+        },
+        onPageLoad: function () {
+            this._super();
+            var _this=this;
+            $('[data-toggle="popover"]',_this.formSelector).popover({
+                trigger: 'hover',
+                html: true
+            });
+
+        },
+
+        /**
+         * 当前对象事件初始化函数
+         */
+        bindEvent : function() {
+            this._super();
+            var _this=this;
+
+
+        },
+
+    });
+
+});
