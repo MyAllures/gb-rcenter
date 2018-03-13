b83675dfde4a4ef6a02d754bd54f03ea8a386db2
ed <edon@dawoo.com>
2018-03-13 14:08:44 +0800

    修改mobile-v3登录

---
+++ WebContent/mobile-v3/js/passport/Login.js
@@ -0,0 +1,93 @@
+//初始化
+$(function () {
+    var options = {
+        /*主页面滚动指定容器，可自行指定范围*/
+        containerScroll: '.mui-content.mui-scroll-wrapper'
+    };
+    muiInit(options);
+});
+
+/**
+ * 立即登录
+ */
+function loginOk(obj,options){
+    var _username = $("#username").val();
+    var _password = $("#password").val();
+    var _captcha = $("#captcha").val();
+    if(_username==""){
+        $("#username-error-msg").html('<i class="mui-icon mui-icon-info"></i>' + window.top.message.passport_auto['用户名不能为空']);
+        $("[name='username']").focus();
+        return;
+    }else{
+        $("#username-error-msg").html("");
+    }
+    if(_password==""){
+        $("#password-error-msg").html('<i class="mui-icon mui-icon-info"></i>' + window.top.message.passport_auto['密码不能为空']);
+        $("[name='password']").focus();
+        return;
+    }else if(_password.length<6){
+        $("#password-error-msg").html('<i class="mui-icon mui-icon-info"></i>' + window.top.message.passport_auto['密码不能少于６位']);
+        $("[name='password']").focus();
+        return;
+    }else{
+        $("#password-error-msg").text("");
+    }
+    $("#loginForm").submit();
+    var $this = $(this);
+    if (_username && _password) {
+        /*mui.ajax(root + "/passport/login.html", {
+            type: "POST",
+            async: false,
+            dataType: "json",
+            data: {
+                "username": _username,
+                "password": _password,
+                "captcha": _captcha
+            },
+            beforeSend: function () {
+                $this.text(window.top.message.passport_auto['登录中']).attr("disabled", "disabled");
+            },
+            success: function (data) {
+                if (data.success) {
+                    sessionStorage.is_login = true;
+                    sessionStorage.isDemo = false;
+                    if(os == 'app_ios'){
+                        loginSucc(_username,_password,1);
+                    }else{
+                        window.location.replace('/mainIndex.html');
+                    }
+                } else {
+                    if (data.isOpenCaptcha) {
+                        mui("#captcha_div")[0].style.display = "block";
+                    }
+                    if (data.message) {
+                        //data.message("用户名或密码错误");
+                        data.message = "用户名或密码错误";
+                        toast(message.passport[data.message] || data.message);
+                    }
+                    if (data.propMessages) {
+                        if (data.propMessages["captcha"]) {
+                            $('._pass').removeClass('final');
+                            $('._captcha').addClass('final');
+                            toast(data.propMessages["captcha"]);
+                        }
+                    }
+                    setTimeout(function () {
+                        $this.text(window.top.message.passport_auto['登录']).removeAttr("disabled");
+                    }, 1000);
+                }
+            },
+            error: function (error) {
+                var data = eval('(' + error.response + ')');
+                if(data.propMessages){
+                    $("#middlePopover").addClass("mui-active");
+                }else{
+                    toast(window.top.message.passport_auto['服务忙']);
+                    setTimeout(function () {
+                        $this.text(window.top.message.passport_auto['登录']).removeAttr("disabled");
+                    }, 1000);
+                }
+            }
+        })*/
+    }
+}
