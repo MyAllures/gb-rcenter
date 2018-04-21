231102e19b4f38cc52a18c11bdb3aa064de193f2
carl <licarl0202@gmail.com>
2018-04-21 16:58:31 +0800

    各个服务-投注记录：修改游戏类型下的api名称的js

--- WebContent/mcenter/js/report/betting/Index.js
+++ WebContent/mcenter/js/report/betting/Index.js
@@ -365,7 +365,7 @@ define(['common/BaseListPage'], function (BaseListPage) {
             });
         },
         /**
-         * checkbox回填 数据格式 [{"apiId":"1","apiType":["01"]},{"apiId":"3","apiType":["01"]}]
+         * checkbox回填 数据格式 [{"apiId":"1","gameType":["01"]},{"apiId":"3","gameType":["01"]}]
          */
         reWrite: function () {
             var selectedJson = $("#apitypeList", parent.document).val();
@@ -381,7 +381,7 @@ define(['common/BaseListPage'], function (BaseListPage) {
                             /*回选api*/
                             $(_that).find("input[name='apicheck']").prop("checked", true);
                             var apiName = $(_that).find("span.search-game-title b").text();
-                            $.each(line.apiType, function (k, v) {
+                            $.each(line.gameType, function (k, v) {
                                 /*回选api下的apiType*/
                                 $(_that).next().children().children("input[type=checkbox]").each(function () {
                                     var gameType = this;
