9ba5e18920635c239c0ba79501c13c1a8fe36154
ed <edon@dawoo.com>
2018-05-04 12:02:29 +0800

    修改mobile-v3优惠申请结束时间

--- WebContent/mobile-v3/js/discounts/ApplyPromoDetail.js
+++ WebContent/mobile-v3/js/discounts/ApplyPromoDetail.js
@@ -146,7 +146,7 @@ function fetchActivityProcess() {
                     html = '';
                 }
                 $('.pro_mone .mui-pull-left').html(proMoneText + '：<span class="color-gray">¥ ' + data.effectivetransaction + '</span>');
-                $('#join .app_num').html('派奖时间：<span class="color-blue">' + formatDate(data.deadLineTime) + '</span>');
+                $('#join .app_num').html('派奖时间：<span class="color-blue">' + data.deadLineTime + '</span>');
                 $('#unCommit .app_num').html('已有 <span class="color-blue">' + addApplyNum(data.ApplyNum) + '</span>人，报名成功');
                 if (data.hasApply) {
                     $('#join').removeClass('mui-hidden');
