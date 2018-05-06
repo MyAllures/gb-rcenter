4a71c70b6d9f4a0752c27e7120a43f74602454d9
martin <martin@mail.com>
2018-05-06 19:03:52 +0800

    捷报数据中心需求:

--- WebContent/mreport/js/Index.js
+++ WebContent/mreport/js/Index.js
@@ -9,15 +9,6 @@ define(['common/BasePage'], function (BasePage) {
          */
         init: function () {
             this.formSelector = "form";
-            this._super();
-            var defaultIndex = "/daily/realTimeSummary.html";
-            if(window.top.location.hash!="") {
-                window.top.topPage.showPage(window.top.location.hash.substr(1));
-                this.lightUp();
-            } else {
-                window.top.location.hash = defaultIndex;
-                this.lightUp();
-            }
         },
 
         /**
@@ -25,25 +16,7 @@ define(['common/BasePage'], function (BasePage) {
          */
         bindEvent: function () {
             this._super();
-            _this = this;
-
-            // 左侧一级菜单收缩
-            $('.list-group-item').click(function() {
-                if($(this).hasClass("active")) {
-                    $(this).removeClass("active");
-                    $(this).children().filter('.hideMenu').stop().slideUp();
-                } else {
-                    $(this).addClass("active").siblings().removeClass("active");
-                    $('.hideMenu').stop().slideUp();
-                    $(this).children().filter('.hideMenu').stop().slideDown();
-                }
-            });
-
-            // 左侧二级菜单点亮
-            $('.hideMenu a').click(function(e) {
-                $(this).parent().addClass("active").siblings().removeClass("active");
-                e.stopPropagation();//阻止事件冒泡
-            });
+            var _this = this;
 
             $('.dropdown-toggle').click(function(e) {
                 $(this).next('.dropdown-menu').stop().slideToggle();
@@ -51,19 +24,6 @@ define(['common/BasePage'], function (BasePage) {
                 $(this).blur(function() {
                     $(this).next('.dropdown-menu').stop().slideUp();
                 });
-            });
-        },
-
-        /**
-         * 点亮菜单
-         */
-        lightUp: function () {
-            var currentPage = window.top.location.hash.substr(1);
-            $("a[nav-target]").each(function() {
-                if($(this).attr("href").indexOf(currentPage)>=0) {
-                    $(this).parent().addClass('active');
-                    return false;
-                }
             });
         }
     });
