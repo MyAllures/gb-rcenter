d1eff68c2ccf065708f22412c18f0320887a2715
gavin <gavin@dawoo.com>
2018-05-17 16:59:05 +0800

    v3 H5添加消息推送功能

---
+++ WebContent/mobile-v3/js/websocket/PopUp.js
@@ -0,0 +1,25 @@
+function MSitePopUp() {}
+
+MSitePopUp.prototype = {
+
+    dialogCallBack: function (data) {
+        var dataObj = $.parseJSON(data);
+        console.info("订阅类型为" + dataObj.subscribeType + "的订阅点收到消息，成功调用回调函数，参数值为" + data);
+        var msgBody = dataObj.msgBody;
+        var content = msgBody.content;
+        var title = msgBody.title;
+        var opt = {
+            title: title,
+            message: content
+        };
+        showDialog(opt);
+    }
+};
+
+/**
+ * 弹框消息提示
+ * @param msg
+ */
+function showDialog(opt) {
+    showWarningMsg(opt.title,opt.message, null, null);
+}
