abbb586116bb7fff770baf768907dd0ff7a0292d
carl <licarl0202@gmail.com>
2018-05-22 10:24:53 +0800

    添加：消息推送插件

--- WebContent/msites/ftl/commonPage/js/websocket/Comet.js
+++ WebContent/msites/ftl/commonPage/js/websocket/Comet.js
@@ -87,8 +87,7 @@ MSiteComet.prototype = {
                 {subscribeType: "SITE_ANN", callBack: popUp.dialogCallBack},
                 {subscribeType: "PCENTER-dialog-Notice", callBack: popUp.dialogCallBack},
                 {subscribeType: "MSITE-Player-Announcement-Notice", callBack: popUp.dialogCallBack},
-                {subscribeType: "MCENTER_READ_COUNT", callBack: popUp.dialogCallBack},
-                {subscribeType: "MSITE-ONLINERECHARGE", callBack: popUp.dialogCallBack},
+                {subscribeType: "MCENTER_READ_COUNT", callBack: popUp.messageCallBack},
                 {subscribeType: "MSITE_DIGICCY_REFRESH_BALANCE", callBack: popUp.dialogCallBack}
             ];
             _this.subscribeMsgs(subscribes);
