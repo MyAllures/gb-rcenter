d374022874bcb11e268c94df19b056e3e021cb57
cherry <cherry1012@mail.com>
2018-05-29 17:39:33 +0800

    ==修复电子支付在切换收款帐号后无法复制问题

--- WebContent/pcenter/js/fund/recharge/ScanElectronic.js
+++ WebContent/pcenter/js/fund/recharge/ScanElectronic.js
@@ -60,11 +60,11 @@ define(['site/fund/recharge/CommonRecharge'], function (BaseEditPage) {
                 $("#electronicDocument").hide();
                 $("#scanDocument").hide();
                 $("#easyPayDocument").show();
-            } else if ("onecodepay" == bankCode){
+            } else if ("onecodepay" == bankCode) {
                 $("#electronicDocument").hide();
                 $("#scanDocument").hide();
                 $("#oneCodePayDocument").show();
-            }else {
+            } else {
                 var depositType = $account.attr("depositType");
                 if (depositType == "scan") {
                     $("#electronicDocument").hide();
@@ -117,6 +117,7 @@ define(['site/fund/recharge/CommonRecharge'], function (BaseEditPage) {
                 $("#accountCode").text(accountCode);
                 $("#bankNum").text(bankNum);
                 $("#bankName").text(bankName);
+                $("#bankName").parent("span").attr("data-clipboard-text", bankName);
                 $("[name=electronicElement]").show();
                 $("#step").text(3);
                 $("[name=scanElement]").hide();
@@ -283,7 +284,7 @@ define(['site/fund/recharge/CommonRecharge'], function (BaseEditPage) {
          * @param e
          * @param option
          */
-        sumFailureCount : function(e, option){
+        sumFailureCount: function (e, option) {
             var _this = this;
             var _window = this.createWin();
             window.top.topPage.newWindow = _window;
@@ -307,7 +308,7 @@ define(['site/fund/recharge/CommonRecharge'], function (BaseEditPage) {
                             _window.close();
                             window.top.topPage.newWindow = null;
                         }
-                    }else{
+                    } else {
                         _this.submit(e, option);
                     }
                     $(e.currentTarget).unlock();
@@ -315,11 +316,11 @@ define(['site/fund/recharge/CommonRecharge'], function (BaseEditPage) {
             });
         },
 
-        closingFailurePrompt:function(e, option){
+        closingFailurePrompt: function (e, option) {
             var _this = this;
             $("#manyFailures").hide();
             $("#backdrop").hide();
-            var _window = window.top.topPage.newWindow ;
+            var _window = window.top.topPage.newWindow;
             if (_window) {
                 _window.close();
                 window.top.topPage.newWindow = null;
@@ -333,8 +334,8 @@ define(['site/fund/recharge/CommonRecharge'], function (BaseEditPage) {
          * @param option
          */
         submit: function (e, option) {
-            var _this = this ;
-            var _window = window.top.topPage.newWindow ;
+            var _this = this;
+            var _window = window.top.topPage.newWindow;
             window.top.topPage.newWindow = null;
             if (!_window) {
                 _window = _this.createWin();
@@ -430,7 +431,7 @@ define(['site/fund/recharge/CommonRecharge'], function (BaseEditPage) {
          */
         changeSale: function () {
             var isOpenActivityHall = $("input[name='isOpenActivityHall']").val();
-            if(isOpenActivityHall == 'false') {
+            if (isOpenActivityHall == 'false') {
                 var amount = $("input[name='result.rechargeAmount']").val();
                 var rechargeType = $("[name='result.rechargeType']").val();
                 var url = root + "/fund/recharge/online/changeScanType.html?rechargeType=" + rechargeType;
